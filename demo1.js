71ae67d7a6b61845ee9507cf650f82480a410ca9
Akash <akash@akash-Lenovo-G560.(none)>
2015-08-15 15:53:30 +0530

    adding my files to my repository

---
+++ demo1.js
@@ -0,0 +1,9 @@
+const myName='akash';
+myName='goluji';
+var myHeading=document.querySelector('h1');
+var typ=typeof myHeading
+document.getElementById('myName').innerHTML=myName;
+
+document.querySelector('html').onclick = function() {
+    alert('Ouch! Stop poking me!');
+}
